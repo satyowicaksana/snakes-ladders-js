b2b192f9bf213666cf7ccf40adaf35b94027e3c1
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-08-30 20:58:15 +0700

    add movePlayer()

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ for(var i = 0; i < 2; i++) {
     }
 }
 
+var leftSideContainer = document.getElementsByClassName('side-container')[0];
+
+var button = document.createElement('button');
+button.setAttribute('onclick', 'movePlayer()');
+button.appendChild(document.createTextNode('ROLL DICE'));
+button.style.float = 'left';
+leftSideContainer.appendChild(button);
+
 var gameContainer = document.createElement('div');
 gameContainer.setAttribute('id', 'game-container');
 document.body.appendChild(gameContainer);
@@ -34,4 +42,43 @@ for(var i = 100; i > 0; i--) {
     
     box.appendChild(document.createTextNode(i));
     gameContainer.appendChild(box);
+}
+
+for(var i = 0; i < 2; i++) {
+    var player = document.createElement('div');
+    player.setAttribute('id', 'player' + (i + 1));
+    document.getElementById('box1').appendChild(player);
+}
+
+var playerPosition = [1, 1]
+var currentPlayer = 1;
+
+function movePlayer() {
+    var diceNumber = Math.ceil(Math.random() * 6);
+    console.log(diceNumber);
+    for(var i = 1; i <= diceNumber; i++){
+        setTimeout(function(){
+            playerPosition[currentPlayer - 1]++;
+            var destinationBox = document.getElementById('box' + playerPosition[currentPlayer - 1]);
+            destinationBox.appendChild(document.getElementById('player' + (currentPlayer)));
+            console.log('move');
+        }, 400 * i);
+    }
+    if(diceNumber != 6){
+        switchPlayer(diceNumber);
+    }
+}
+
+function switchPlayer(timeout) {
+    setTimeout(function() {
+        console.log('masukswitch')
+        if(currentPlayer === 1) {
+            currentPlayer = 2;
+            button.style.background = '#f76c6c';
+        } else {
+            currentPlayer = 1;
+            button.style.background = '#f8e9a1';
+        }
+        console.log(currentPlayer);
+    }, 400 * timeout + 1);
 }
